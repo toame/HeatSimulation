ec2e59aafac085374ff02e92421d451838964b2f
toame <m21j4012c@edu.tottori-u.ac.jp>
2021-12-25 15:11:44 +0900

    リファクタリング

--- Molecular_dynamics_cuda.cu
+++ Molecular_dynamics_cuda.cu
@@ -34,7 +34,7 @@ void setCurand(unsigned long long seed, curandState *state){
     curand_init(seed, i_global, 0, &state[i_global]);
 }
 __global__
-void qinit(double *q1, double *q0, const int size_)
+void qinit(float *q1, float *q0, const int size_)
 {
     const int thread_id = threadIdx.x+blockDim.x*blockIdx.x;
     const int i = thread_id;
@@ -43,64 +43,45 @@ void qinit(double *q1, double *q0, const int size_)
     }
 }
 __global__
-void statistics_reset_d(double* fluxC, const int model_size) {
+void statistics_reset_d(float* fluxC, const int model_size) {
     const int thread_id = threadIdx.x+blockDim.x*blockIdx.x;
     const int i = thread_id + 1;
     if(i < model_size)
-        fluxC[i] = 3.5;
+        fluxC[i] = 0.0;
 }
 
 __global__
-void statistics_reset_test(double* fluxC, curandState *state, const int model_size) {
-    const int thread_id = threadIdx.x+blockDim.x*blockIdx.x;
-    const int i = thread_id + 1;
-    if(i < model_size)
-        fluxC[i] = curand_normal(&state[i]);
-}
-
-__global__
-void Update(double* q2, double* q1, double *q0,curandState *state,  double* ct_c, double *fluxC,double *temperature_plot, const int n1_L, const int n1_R, const int n2_L, const int n2_R, const int n3_L, const int n3_R, const bool is_heat)
+void Update(float* q2, const float* q1, const float *q0,curandState *state,  float* ct_c, float *fluxC,float *temperature_plot, const int n1_L, const int n1_R, const int n2_L, const int n2_R, const int n3_L, const int n3_R, const bool is_heat)
 {
     const int thread_id = threadIdx.x+blockDim.x*blockIdx.x;
     const int i = thread_id + 1;
-    if (i < n3_R) {
-        for (int k = 0; k < statistics_interval; k++) {
-            // 次の粒子状態の計算
-            const int is_middleL = (n2_L < i && i <= n2_R);
-            const int is_middleR = (n2_L <= i && i < n2_R);
-            const double muL = is_middleL * mu1 + !(is_middleL) * mu1_bath;
-            const double muR = is_middleR * mu1 + !(is_middleR) * mu1_bath;
 
-            const double f_R = muR * (q1[i + 1] - q1[i]);
-            const double f_L = muL * (q1[i] - q1[i - 1]);
-            const double l_f = f_R - f_L;
-            
-            const double nf_R = (is_middleR) * beta0 * (q1[i + 1] - q1[i]) * (q1[i + 1] - q1[i]) * (q1[i + 1] - q1[i]);
-            const double nf_L = (is_middleL) * beta0 * (q1[i - 1] - q1[i]) * (q1[i - 1] - q1[i]) * (q1[i - 1] - q1[i]);
-            const double nl_f = nf_L + nf_R;
+    const int is_middleL = (n2_L < i && i <= n2_R);
+    const int is_middleR = (n2_L <= i && i < n2_R);
+    
+    const float muL = is_middleL * mu1 + !(is_middleL) * mu1_bath;
+    const float muR = is_middleR * mu1 + !(is_middleR) * mu1_bath;
 
-            const double dq1 = q1[i] - q0[i];
-            const double random_f = (is_heat || i <= n1_R || i >= n3_L) ? (-gamma_t * dq1 + ct_c[i] * curand_normal(&state[i])) : 0;
-            
-            const double f = l_f + nl_f - mu0 * q1[i] + random_f;
-            const double dq2 = dq1 + f * dt2 + random_f;  
+    const float f_R = muR * (q1[i + 1] - q1[i]);
+    const float f_L = muL * (q1[i] - q1[i - 1]);
+    const float l_f = f_R - f_L;
 
-            q2[i] = q1[i] + dq2;
+    const float nf_R = (is_middleR) * beta0 * (q1[i + 1] - q1[i]) * (q1[i + 1] - q1[i]) * (q1[i + 1] - q1[i]);
+    const float nf_L = (is_middleL) * beta0 * (q1[i - 1] - q1[i]) * (q1[i - 1] - q1[i]) * (q1[i - 1] - q1[i]);
+    const float nl_f = nf_L + nf_R;
 
-            // 統計量の計算
-            const double p = 0.5 * (q2[i] - q0[i]) * dt_1;
-            fluxC[i] += p * (-f_L + nf_L);
-            temperature_plot[i] += p * p;
-            
-            // __syncthreads();
-            // if(thread_id == 0) {
-            //     q0 = q1;
-            //     q1 = q2;
-            //     q2 = q0;
-            // }
-            // __syncthreads();
-        }
-    }
+    const float dq1 = q1[i] - q0[i];
+    const float random_f = (is_heat || i <= n1_R || i >= n3_L) ? (-gamma_t * dq1 + ct_c[i] * curand_normal(&state[i])) : 0;
+
+    const float f = l_f + nl_f - mu0 * q1[i];
+    const float dq2 = dq1 + f * dt2 + random_f;  
+
+    q2[i] = q1[i] + dq2;
+
+    // 統計量の計算
+    const float p = 0.5 * (q2[i] - q0[i]) * dt_1;
+    fluxC[i] += p * (-f_L + nf_L);
+    temperature_plot[i] += p * p;
 }
 
 class FPUT_Lattice_1D{
@@ -119,26 +100,24 @@ private:
     long long int initialStateStep;         // 統計量を測定開始するステップ数（最初は収束していないため）
     long long int Step;                     // 全体のステップ数
 
-    const double temp_h = 1.2;              // 高温熱浴の温度
-    const double temp_l = 0.8;              // 低温熱浴の温度
+    const float temp_h = 1.2;              // 高温熱浴の温度
+    const float temp_l = 0.8;              // 低温熱浴の温度
 
+    const float c_h = sqrt(2.0*gamma0*temp_h*dt);  // 高温側の分散
+    const float c_l = sqrt(2.0*gamma0*temp_l*dt);  // 低温側の分散
     
-    const double c_h = sqrt(2.0*gamma0*temp_h*dt);  // 高温側の分散
-    const double c_l = sqrt(2.0*gamma0*temp_l*dt);  // 低温側の分散
-    
-    
-    const double ct_h = c_h * dt;           
-	const double ct_l = c_l * dt;        
-    double* h_ct_c;           // 中央部分の（初期）温度  
-    double* d_ct_c;           // 中央部分の（初期）温度    
+    const float ct_h = c_h * dt;           
+	const float ct_l = c_l * dt;        
+    float* h_ct_c;           // 中央部分の（初期）温度  
+    float* d_ct_c;           // 中央部分の（初期）温度    
 
-    double* q0;                         // 2ステップ前の粒子位置
-    double* q1;                         // 1ステップ前の粒子位置
-    double* q2;                         // 現在の粒子位置    
-    double* h_fluxC;
-    double* h_temperature_plot;
-    double* d_fluxC;
-    double* d_temperature_plot;
+    float* q0;                         // 2ステップ前の粒子位置
+    float* q1;                         // 1ステップ前の粒子位置
+    float* q2;                         // 現在の粒子位置    
+    float* h_fluxC;
+    float* h_temperature_plot;
+    float* d_fluxC;
+    float* d_temperature_plot;
 
     long long int count_flux = 0;
     long long int count_temp = 0;
@@ -160,35 +139,35 @@ public:
         n3_R = n3_L + HeatBath_size - 1;
         model_size = n3_R + 2;
         cerr << n1_L << "," << n1_R << "," << n2_L << "," << n2_R << "," << n3_L << "," << n3_R <<"," << model_size <<  endl;
-        h_ct_c = (double *)malloc(sizeof(double) * model_size);
-        h_fluxC = (double *)malloc(sizeof(double) * model_size);
-        h_temperature_plot = (double *)malloc(sizeof(double) * model_size);
-        cudaMalloc(&d_ct_c, sizeof(double) * model_size);
-        cudaMalloc(&d_fluxC, sizeof(double) * model_size);
-        cudaMalloc(&d_temperature_plot, sizeof(double) * model_size);
+        h_ct_c = (float *)malloc(sizeof(float)* (model_size + 256));
+        h_fluxC = (float *)malloc(sizeof(float) * (model_size + 256));
+        h_temperature_plot = (float *)malloc(sizeof(float) * (model_size + 256));
+        cudaMalloc(&d_ct_c, sizeof(float) * (model_size + 256));
+        cudaMalloc(&d_fluxC, sizeof(float) * (model_size + 256));
+        cudaMalloc(&d_temperature_plot, sizeof(float) * model_size);
 
-        cudaMalloc(&q0, sizeof(double) * model_size);
-        cudaMalloc(&q1, sizeof(double) * model_size);
-        cudaMalloc(&q2, sizeof(double) * model_size);
-        cudaMalloc(&state,  sizeof(double) * model_size * sizeof(curandState));
+        cudaMalloc(&q0, sizeof(float) * (model_size + 256));
+        cudaMalloc(&q1, sizeof(float) * (model_size + 256));
+        cudaMalloc(&q2, sizeof(float) * (model_size + 256));
+        cudaMalloc(&state,  sizeof(float) * (model_size + 256) * sizeof(curandState));
         for(int i = n1_L; i <= n1_R; i++) {
             h_ct_c[i] = ct_h;
         }
         for(int i = n2_L; i <= n2_R; i++) {
-            const double temp_c = temp_h - (temp_h - temp_l) * (i - n2_L)/(n2_R - n2_L + 1);
-            const double c_c = sqrt(2.0*gamma0*temp_c*dt); 
+            const float temp_c = temp_h - (temp_h - temp_l) * (i - n2_L)/(n2_R - n2_L + 1);
+            const float c_c = sqrt(2.0*gamma0*temp_c*dt); 
             h_ct_c[i] = c_c * dt;
         }
         for(int i = n3_L; i <= n3_R; i++) {
             h_ct_c[i] = ct_l;
         }
-        cudaMemcpy(d_ct_c, h_ct_c, sizeof(double) * model_size, cudaMemcpyHostToDevice);
+        cudaMemcpy(d_ct_c, h_ct_c, sizeof(float) * model_size, cudaMemcpyHostToDevice);
         dim3 grid_size = dim3(model_size / blockSize + 1, 1, 1);
         dim3 block_size = dim3(blockSize, 1, 1);
         setCurand<<<grid_size,block_size>>>(1, state);
         qinit<<<grid_size,block_size>>>(q0, q1, model_size);
     }
-    void settingStep(const int HeatSimulation_, const int initialStateStep_, const int Step_) {
+    void settingStep(const long long int HeatSimulation_, const long long int initialStateStep_, const long long int Step_) {
         HeatSimulation = HeatSimulation_;
         initialStateStep = initialStateStep_;
         Step = Step_;
@@ -196,17 +175,17 @@ public:
     // 進行度の出力
     void showProcessing() {
         // end = std::chrono::system_clock::now();
-        // double time = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0 / 1000.0);
-        // std::cerr << "process = " << std::fixed << std::setprecision(3) << (double)stepCount/(double)allSteps * 100.0 << "%, ";
+        // float time = static_cast<float>(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0 / 1000.0);
+        // std::cerr << "process = " << std::fixed << std::setprecision(3) << (float)stepCount/(float)allSteps * 100.0 << "%, ";
         // std::cerr << "StepCount = " << std::setfill('0') << std::right << std::setw(9) << stepCount << "/" << std::setfill('0') << std::right << std::setw(9) << allSteps << ", ";
         // std::cerr << "t = " << std::fixed << std::setprecision(6) << time/86400.0 << " [days], ";
-        // std::cerr << "est. = " << std::fixed << std::setprecision(6) << time/((double)stepCount/(double)allSteps)/86400.0<< " [days]" << std::endl;
-        double *h_q0L = (double*) malloc(sizeof(double));
-        double *h_q0M = (double*) malloc(sizeof(double));
-        double *h_q0R = (double*) malloc(sizeof(double));
-        cudaMemcpy(h_q0L, q0 + (n1_L + n1_R) / 2, sizeof(double), cudaMemcpyDeviceToHost);
-        cudaMemcpy(h_q0M, q0 + middle_size / 2, sizeof(double), cudaMemcpyDeviceToHost);
-        cudaMemcpy(h_q0R, q0 + (n3_L + n3_R) / 2, sizeof(double), cudaMemcpyDeviceToHost);
+        // std::cerr << "est. = " << std::fixed << std::setprecision(6) << time/((float)stepCount/(float)allSteps)/86400.0<< " [days]" << std::endl;
+        float *h_q0L = (float*) malloc(sizeof(float));
+        float *h_q0M = (float*) malloc(sizeof(float));
+        float *h_q0R = (float*) malloc(sizeof(float));
+        cudaMemcpy(h_q0L, q0 + (n1_L + n1_R) / 2, sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_q0M, q0 + middle_size / 2, sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_q0R, q0 + (n3_L + n3_R) / 2, sizeof(float), cudaMemcpyDeviceToHost);
         std::cerr << *h_q0L << " " << *h_q0M << " " << *h_q0R << std::endl;
         assert(*h_q0M < 1e+10);
 
@@ -214,7 +193,7 @@ public:
     }
 
     void step() {
-        //void Update(double *q2, double* q1, double *q0, double ct_c, double *fluxC, curandState *state, double *temperature_plot, const int n1_L, const int n1_R, const int n2_L, const int n2_R, const int n3_L, const int n3_R, const bool is_heat, const double *f_nl)
+        //void Update(float *q2, float* q1, float *q0, float ct_c, float *fluxC, curandState *state, float *temperature_plot, const int n1_L, const int n1_R, const int n2_L, const int n2_R, const int n3_L, const int n3_R, const bool is_heat, const float *f_nl)
         dim3 grid_size = dim3(model_size / blockSize + 1, 1, 1);
         dim3 block_size = dim3(blockSize, 1, 1);
         Update<<<grid_size,block_size>>>(q2, q1, q0, state,d_ct_c, d_fluxC, d_temperature_plot, n1_L, n1_R, n2_L, n2_R, n3_L, n3_R, stepCount < HeatSimulation);
@@ -239,15 +218,15 @@ public:
         dim3 grid_size = dim3(model_size / blockSize + 1, 1, 1);
         dim3 block_size = dim3(blockSize, 1, 1);
         //statistics_reset_test<<<grid_size,block_size>>>(d_fluxC, state, model_size);
-        cudaMemcpy(h_fluxC, d_fluxC, sizeof(double) * model_size, cudaMemcpyDeviceToHost);
-        double sum_flux = 0.0;
+        cudaMemcpy(h_fluxC, d_fluxC, sizeof(float) * model_size, cudaMemcpyDeviceToHost);
+        float sum_flux = 0.0;
         int count0 = 0;
         for(int i = n2_L + 1; i < n2_R; i++) {
             sum_flux += h_fluxC[i];
             count0++;
         }
-        const double ave_flux = sum_flux / count0 / count_flux;
-        const double ave_kappa = ave_flux / (temp_h - temp_l) * middle_size;
+        const float ave_flux = sum_flux / count0 / count_flux;
+        const float ave_kappa = ave_flux / (temp_h - temp_l) * middle_size;
         cerr << stepCount << "," << sum_flux << "," << ave_kappa << "," << count_flux << ",";
 
     }
@@ -272,10 +251,10 @@ int main(void) {
         if(i == 10000)
             model.statistics_reset();
         model.step();
-        if((i + 1) % (500000/statistics_interval) == 0) {
+        if((i + 1) % (50000/statistics_interval) == 0) {
             model.showProcessing();
             end = std::chrono::system_clock::now();  // 計測終了時間
-            double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); //処理に要した時間をミリ秒に変換
+            float elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); //処理に要した時間をミリ秒に変換
             model.output_Kappa();
             cerr << elapsed/1000.0/60.0/60.0 << "[h]" << "," << elapsed/1000.0/60.0/60.0/i * 5000000000/statistics_interval << "[h]" << endl;
         }
