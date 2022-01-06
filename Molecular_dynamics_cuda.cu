fd190123e2fd883f8bec2701e079b535672ecc18
toame <tomo2073_41@yahoo.co.jp>
2022-01-06 18:59:39 +0900

    温度分布処理修正

--- Molecular_dynamics_cuda.cu
+++ Molecular_dynamics_cuda.cu
@@ -256,7 +256,7 @@ public:
                 if(v.size() < 2) continue;
                 const int i = stoi(v[0]);
                 const double temp_ic = stod(v[1]);
-                if(i >= n2_L && i <= n2_R) {
+                if((i - n2_L) * 2 + n2_L >= n2_L && i <= (i - n2_L) * 2 + n2_L + 1) {
                     h_ct_c[(i - n2_L) * 2 + n2_L] = sqrt(2.0 * gamma0 * temp_ic * dt) * dt;
                     h_ct_c[(i - n2_L) * 2 + n2_L + 1] = sqrt(2.0 * gamma0 * temp_ic * dt) * dt;
                 }
