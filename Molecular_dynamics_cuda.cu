a87a24c1d024725b85a6040767c32ba4ebe406d3
toame <tomo2073_41@yahoo.co.jp>
2022-01-07 15:02:41 +0900

    温度分布処理修正

--- Molecular_dynamics_cuda.cu
+++ Molecular_dynamics_cuda.cu
@@ -250,16 +250,19 @@ public:
             while (getline(inputTemperature, str)) {
                 std::vector<std::string> v; 
                 std::stringstream ss{str};             
-                while ( getline(ss, s, ',') ){    
+                while (getline(ss, s, ' ') ){    
                     v.push_back(s);
                 }
                 if(v.size() < 2) continue;
                 const int i = stoi(v[0]);
                 const double temp_ic = stod(v[1]);
-                if((i - n2_L) * 2 + n2_L >= n2_L && i <= (i - n2_L) * 2 + n2_L + 1) {
+                if(n2_L <= (i - n2_L) * 2 + n2_L&& (i - n2_L) * 2 + n2_L + 1 <= n2_R) {
                     h_ct_c[(i - n2_L) * 2 + n2_L] = sqrt(2.0 * gamma0 * temp_ic * dt) * dt;
                     h_ct_c[(i - n2_L) * 2 + n2_L + 1] = sqrt(2.0 * gamma0 * temp_ic * dt) * dt;
+                    if (i*2 <= n2_L + 20 || i*2 >= n2_R - 20)
+                    cerr << i << "," << temp_ic << "," << (i - n2_L) * 2 + n2_L << "," << (i - n2_L) * 2 + n2_L + 1 << endl;
                 }
+                
             }
             std::cerr << "previous_ok" << std::endl;
         }
